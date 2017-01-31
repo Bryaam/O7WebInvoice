783a75ccc8ed96e8af0c9fb7446dbe54163cf310
Gianfranco <a20121776@pucp.pe>
2017-01-31 15:44:35 -0500

    c

--- 1 Interface Layer/Angkor.O7Web.Interface.Finantial.Controller/InvoiceController.cs
+++ 1 Interface Layer/Angkor.O7Web.Interface.Finantial.Controller/InvoiceController.cs
@@ -86,6 +86,7 @@ namespace Angkor.O7Web.Interface.Finantial.Controller
             ViewData["impPerc"] = head[0].impPer;
             ViewData["impTot"] = head[0].impTot;
             ViewData["estado"] = head[0].estado;
+            ViewData["estadoDesc"] = head[0].estadoDesc;
 
             var responseDetail = domain.GetInvoiceDetailView(User.Company, User.Branch, documentType, documentId);
             var detail = ((O7SuccessResponse<List<InvoiceDetailView>>)responseDetail).Value1;//new O7JsonResult(response);
