9ae00cb1d287bfa6567e52af14d863312a010b44
Lourdes <montalvolujan12@outlook.com>
2017-02-06 17:53:36 -0500

    c

--- 1 Interface Layer/Angkor.O7Web.Interface.Finantial/Scripts/Client/Managment.function.js
+++ 1 Interface Layer/Angkor.O7Web.Interface.Finantial/Scripts/Client/Managment.function.js
@@ -1,108 +1,98 @@
-﻿function getAutoCompleteDataSingle(id, requestUrl) {
+﻿function getAutoCompleteDataSingle(id,hiddenId, requestUrl) {
     $.ajax({ method: "GET", url: requestUrl, async: false })
-        .done(function (resultField) {
-            resultField = resultField.replace(/Description/g, 'name');
-            resultField = resultField.replace(/Id/g, 'id');
-            var objResultPostale = jQuery.parseJSON(resultField);
-            $(id).typeahead({ source: objResultPostale });
-            }).fail(function (result) {
-                 toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-            });
+    .done(function (resultField) {
+        var objResult = jQuery.parseJSON(parseAutocomplete(resultField));
+        var autocomplete = $(id).typeahead({ source: objResult });
+        if (hiddenId != null) {
+            autocomplete.afterSelect = function () {
+                var field = $(id).typeahead("getActive");
+                $(hiddenId).val(field.id);
+            }
+        }
+        //if (hiddenId==null) {
+         //   $(id).typeahead({ source: objResult });
+       // } else {
+           // $(id).typeahead({
+               // source: objResult,
+               // afterSelect: function () {
+                //    var field = $(id).typeahead("getActive");
+              //      $(hiddenId).val(field.id);
+            //    }
+          //  });
+        //}
+    }).fail(function (result) {
+        toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+    });
 }
-
-function getAutoCompleteDataNested() {
+function toggleFields(status) {
+    for (i = 1; i < arguments.length; i++) {
+        if (status == "enabled") {
+            $(arguments[i]).removeAttr("disabled");
+        } else {
+            $(arguments[i]).attr("disabled", "disabled");
+        }
+    }
+}
+function getAutoCompleteDataNested(counAuto,depAuto,proAuto,disAuto,zoneAuto) {
     $.ajax({ method: "GET", url: "/Finantial/Client/AllCountries", async: false }).done(function (resultCountry) {
-        resultCountry = resultCountry.replace(/Description/g, 'name');
-        resultCountry = resultCountry.replace(/Id/g, 'id');
-        var objResultCountry = jQuery.parseJSON(resultCountry);
-        $(countryAutocomplete).typeahead({
+        var objResultCountry = jQuery.parseJSON(parseAutocomplete(resultCountry));
+        $(counAuto).typeahead({
             source: objResultCountry,
             afterSelect: function () {
-                var country = $(countryAutocomplete).typeahead("getActive");
+                var country = $(counAuto).typeahead("getActive");
                 $(countryId).val(country.id);
                 $.ajax({ method: "GET", url: "/Finantial/Client/ValidateCountryEntry", data: { countryId: country.id }, async: false })
-                    .done(function (resultValidation) {
-                        var objResultValidation = jQuery.parseJSON(resultValidation.toLowerCase());
-                        if (objResultValidation == true) {
-                            $(departmentAutocomplete).removeAttr("disabled");
-                            $(provinceAutocomplete).removeAttr("disabled");
-                            $(districtAutocomplete).removeAttr("disabled");
-                            $(zoneAutocomplete).removeAttr("disabled");
-                        } else {
-                            $(departmentAutocomplete).attr("disabled", "disabled");
-                            $(provinceAutocomplete).attr("disabled", "disabled");
-                            $(districtAutocomplete).attr("disabled", "disabled");
-                            $(zoneAutocomplete).attr("disabled", "disabled");
-                        }
-                    }).fail(function (result) {
-                        toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-                    });
+                .done(function (resultValidation) {
+                    var objResultValidation = jQuery.parseJSON(resultValidation.toLowerCase());
+                    if (objResultValidation == true) {
+                        toggleFields("enabled", depAuto, proAuto, disAuto, zoneAuto);
+                    } else {
+                        toggleFields("disabled", depAuto, proAuto, disAuto, zoneAuto);
+                    }
+                }).fail(function (result) {
+                    toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+                });
+                getAutoCompleteDataSingle(zoneAutocomplete,zoneId,"/Finantial/Client/AllClientZones");
 
-                    $.ajax({ method: "GET", url: "/Finantial/Client/AllClientZones", data: { countryId: country.id }, async: false })
-                        .done(function (resultZone) {
-                            resultZone = resultZone.replace(/Description/g, 'name');
-                            resultZone = resultZone.replace(/Id/g, 'id');
-                            var objResultZone = jQuery.parseJSON(resultZone);
-                            $(zoneAutocomplete).typeahead({
-                                source: objResultZone,
-                                afterSelect: function () {
-                                    var zone = $(zoneAutocomplete).typeahead("getActive");
-                                    $(zoneId).val(zone.id)
-                                }
-                            });
-                        }).fail(function (result) {
-                            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-                        });
-                        $.ajax({ method: "GET", url: "/Finantial/Client/AllDepartments", data: { countryId: country.id }, async: false })
-                            .done(function (result) {
-                                result = result.replace(/Description/g, 'name');
-                                result = result.replace(/Id/g, 'id');
-                                var objResult = jQuery.parseJSON(result);
-                                $(departmentAutocomplete).typeahead({
-                                    source: objResult,
+                $.ajax({ method: "GET", url: "/Finantial/Client/AllDepartments", data: { countryId: country.id }, async: false })
+                .done(function (result) {
+                    var objResult = jQuery.parseJSON(parseAutocomplete(result));
+                    $(departmentAutocomplete).typeahead({
+                        source: objResult,
+                        afterSelect: function () {
+                        var department = $(departmentAutocomplete).typeahead("getActive");
+                        $(departmentId).val(department.id);
+                        $.ajax({ method: "GET", url: "/Finantial/Client/AllProvinces", data: { countryId: country.id, departmentId: department.id }, async: false })
+                            .done(function (resultProv) {
+                                var objResultProv = jQuery.parseJSON(parseAutocomplete(resultProv));
+                                $(provinceAutocomplete).typeahead({
+                                    source: objResultProv,
                                     afterSelect: function () {
-                                    var department = $(departmentAutocomplete).typeahead("getActive");
-                                    $(departmentId).val(department.id);
-                                    $.ajax({ method: "GET", url: "/Finantial/Client/AllProvinces", data: { countryId: country.id, departmentId: department.id }, async: false })
-                                        .done(function (resultProv) {
-                                            resultProv = resultProv.replace(/Description/g, 'name');
-                                            resultProv = resultProv.replace(/Id/g, 'id');
-                                            var objResultProv = jQuery.parseJSON(resultProv);
-                                            $(provinceAutocomplete).typeahead({
-                                                source: objResultProv,
-                                                afterSelect: function () {
-                                                    var province = $(provinceAutocomplete).typeahead("getActive");
-                                                    $(provinceId).val(province.id);
-                                                    $.ajax({ method: "GET", url: "/Finantial/Client/AllDistricts", data: { countryId: country.id, departmentId: department.id, provinceId: province.id }, async: false })
-                                                        .done(function (resultDistrict) {
-                                                            resultDistrict = resultDistrict.replace(/Description/g, 'name');
-                                                            resultDistrict = resultDistrict.replace(/Id/g, 'id');
-                                                            var objResultDis = jQuery.parseJSON(resultDistrict);
-                                                            $(districtAutocomplete).typeahead({
-                                                                source: objResultDis,
-                                                                afterSelect: function () {
-                                                                    var district = $(districtAutocomplete).typeahead("getActive");
-                                                                    $(districtId).val(district.id);
-                                                                }
-                                                            });
-                                                        }).fail(function (result) {
-                                                            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-                                                        });
-                                                }
-                                            });
-                                        }).fail(function (result) {
-                                            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-                                        });
+                                        var province = $(provinceAutocomplete).typeahead("getActive");
+                                        $(provinceId).val(province.id);
+                                        getAutoCompleteDataSingle(districtAutocomplete,districtId,"/Finantial/Client/AllDistricts");
                                     }
                                 });
                             }).fail(function (result) {
                                 toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
                             });
-           }
-                        });
-                    }).fail(function (result) {
-                        toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+                        }
                     });
+
+                }).fail(function (result) {
+                    toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+                });
+            }
+        });
+    }).fail(function (result) {
+        toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+    });
+}
+
+function parseAutocomplete(json) {
+    json = json.replace(/Description/g, 'name');
+    json = json.replace(/Id/g, 'id');
+    return json;
 }
 
 function rowValidate(i,tblName,curName, buttonName) {
@@ -129,3 +119,4 @@ function iterate_Combo(documentType, objResult) {
     });
     documentType.trigger("chosen:updated");
 }
+
