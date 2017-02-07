65df834eb72ed017b12c7a7cf106b9e86561a0cc
Lourdes <montalvolujan12@outlook.com>
2017-02-07 10:38:32 -0500

    managment initialice

---
+++ 1 Interface Layer/Angkor.O7Web.Interface.Finantial/Scripts/Client/Managment.initialize.js
@@ -0,0 +1,16 @@
+﻿function initialize() {
+    moment.locale('es');
+
+    $("#hddCountReciber").val("0");
+    $("#hddCountInvoicer").val("0");
+
+    $("#dpckRegister").datepicker({
+        format: "dd/mm/yyyy"
+    });
+
+    $("#chkDepartment").chosen({
+        width: "100%"
+    });
+
+    var action = $("#txtAction").val();
+}
