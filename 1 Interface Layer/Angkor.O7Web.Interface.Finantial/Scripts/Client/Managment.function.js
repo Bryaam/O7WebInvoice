55ac8539cee326c1ffc2a81eebc2344dd55bac7a
Lourdes <montalvolujan12@outlook.com>
2017-02-07 11:54:52 -0500

    managment initialize

--- 1 Interface Layer/Angkor.O7Web.Interface.Finantial/Scripts/Client/Managment.function.js
+++ 1 Interface Layer/Angkor.O7Web.Interface.Finantial/Scripts/Client/Managment.function.js
@@ -124,59 +124,7 @@ function toolAutoComplete(nameAutoComplete, objResultDis, name) {
     });
 }
 
-function get_allClientZone() {
-    $.ajax({ method: "GET", url: "/Finantial/Client/AllClientZones", data: { countryId: $("#chkCountry").val() }, async: false })
-        .done(function (result) {
-            var objResult = jQuery.parseJSON(result);
-            var documentType = $("#chkZone");
-            documentType.html("");
-            iterate_Combo(documentType, objResult);
-        }).fail(function (result) {
-            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-        });
-}
 
-function get_allClientDistrict() {
-    $.ajax({ method: "GET", url: "/Finantial/Client/AllDistricts", data: { countryId: $("#chkCountry").val(), departmentId: $("#chkDepartment").val(), provinceId: $(this).val() }, async: false })
-            .done(function (result) {
-                var objResult = jQuery.parseJSON(result);
-                var documentType = $("#chkDistrict");
-                documentType.html("");
-                iterate_Combo(documentType, objResult);
-            }).fail(function (result) {
-                toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-            });
-}
-
-function get_allClientProvinces() {
-    $.ajax({ method: "GET", url: "/Finantial/Client/AllProvinces", data: { countryId: $("#chkCountry").val(), departmentId: $(this).val() }, async: false })
-             .done(function (result) {
-                 var objResult = jQuery.parseJSON(result);
-                 var documentType = $("#chkProvince");
-                 documentType.html("");
-                 iterate_Combo(documentType, objResult);
-             }).fail(function (result) {
-                 toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-             });
-}
-
-function get_allClientDepartments() {
-    $.ajax({ method: "GET", url: "/Finantial/Client/AllDepartments", data: { countryId: $("#chkCountry").val() }, async: false })
-           .done(function (result) {
-               var objResult = jQuery.parseJSON(result);
-               var documentType = $("#chkDepartment");
-               documentType.html("");
-               iterate_Combo(documentType, objResult);
-           }).fail(function (result) {
-               toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
-           });
-}
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> a64a624b24b5760e5f3e587899f34889b3d293e7
 function generateAddressFields(row,reciber_count) {
     row.CodDir = "";
     row.Address = "<input class='form-control' style='width:100%' name='txtInvoicerAddress_" + reciber_count + "'>";
--- 1 Interface Layer/Angkor.O7Web.Interface.Finantial/Scripts/Client/Managment.initialize.js
+++ 1 Interface Layer/Angkor.O7Web.Interface.Finantial/Scripts/Client/Managment.initialize.js
@@ -13,4 +13,210 @@
     });
 
     var action = $("#txtAction").val();
+
+    if (action == "3") {
+        $("#btnSubmit").hide();
+    }
+
+    $("#dpckRegister").val(moment().format("DD/MM/YYYY"));
+
+    $("#chkOriginClient").chosen({
+        width: "100%",
+        disable_search: true
+    });
+
+    $("#chkClientState").chosen({
+        width: "100%",
+        disable_search: true
+    });
+
+    $("#chkpersonType").chosen({
+        width: "100%",
+        disable_search: true
+    });
+
+    $("#chkDocumentType").chosen({
+        width: "100%",
+        disable_search: true
+    });
+
+    $("#chkCountry").chosen({
+        width: "100%"
+    });
+
+    $("#chkZone").chosen({
+        width: "100%"
+    });
+
+    $("#chkPostale").chosen({
+        width: "100%"
+    });
+
+    $("#chkProvince").chosen({
+        width: "100%"
+    });
+
+    $("#chkDistrict").chosen({
+        width: "100%"
+    });
+
+    on_Combos();
+
+    if (action == "1") {
+        get_allClientProvinces();
+        get_allClientDistrict();
+    }
+}
+
+function on_Combos() {
+    get_allPersonType();
+    get_allCountries();
+    get_allClientZone();
+
+    $("#chkCountry").on('change', function (e) {
+        get_allClientZone();
+        get_allClientDepartments(1);
+        get_allClientDistrict();
+        get_allClientProvinces();
+    });
+
+    $("#chkProvince").on('change', function (e) {
+        get_allClientDistrict();
+    });
+
+    $("#chkDepartment").on('change', function (e) {
+        get_allClientProvinces();
+    });
+
+    get_allPostales();
+    get_allClientOrigins();
+    get_allClientStates();
+    get_allClientType();
+    get_allClientDepartments(0);
+}
+
+
+function get_allPersonType() {
+    $('#chkpersonType').on('change', function (e) {
+        var personType = $(this).val();
+
+        if (personType == "N") $("#divRuc").removeAttr("style");
+        else $("#divRuc").attr("style", "visibility:hidden;");
+
+        $.ajax({ method: "GET", url: "/Finantial/Client/DocumentType", data: { clientType: personType }, async: false })
+            .done(function (result) {
+                var objResult = jQuery.parseJSON(result);
+                var documentType = $("#chkDocumentType");
+                documentType.html("");
+                iterate_Combo(documentType, objResult);
+            }).fail(function (result) {
+                toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+            });
+    });
+}
+
+function get_allCountries() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/AllCountries", async: false })
+        .done(function (result) {
+            var objResult = jQuery.parseJSON(result);
+            var documentType = $("#chkCountry");
+            iterate_Combo(documentType, objResult);
+        }).fail(function (result) {
+            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+        });
+}
+
+function get_allClientZone() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/AllClientZones", data: { countryId: $("#chkCountry").val() }, async: false })
+        .done(function (result) {
+            var objResult = jQuery.parseJSON(result);
+            var documentType = $("#chkZone");
+            documentType.html("");
+            iterate_Combo(documentType, objResult);
+        }).fail(function (result) {
+            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+        });
+}
+
+function get_allClientDistrict() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/AllDistricts", data: { countryId: $("#chkCountry").val(), departmentId: $("#chkDepartment").val(), provinceId: $(this).val() }, async: false })
+            .done(function (result) {
+                var objResult = jQuery.parseJSON(result);
+                var documentType = $("#chkDistrict");
+                documentType.html("");
+                iterate_Combo(documentType, objResult);
+            }).fail(function (result) {
+                toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+            });
+}
+
+function get_allClientProvinces() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/AllProvinces", data: { countryId: $("#chkCountry").val(), departmentId: $(this).val() }, async: false })
+             .done(function (result) {
+                 var objResult = jQuery.parseJSON(result);
+                 var documentType = $("#chkProvince");
+                 documentType.html("");
+                 iterate_Combo(documentType, objResult);
+             }).fail(function (result) {
+                 toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+             });
+}
+
+function get_allClientDepartments(flag) {
+    $.ajax({ method: "GET", url: "/Finantial/Client/AllDepartments", data: { countryId: $("#chkCountry").val() }, async: false })
+           .done(function (result) {
+               var objResult = jQuery.parseJSON(result);
+               var documentType = $("#chkDepartment");
+               if (flag == 1) {
+                   documentType.html("");
+               }
+               iterate_Combo(documentType, objResult);
+           }).fail(function (result) {
+               toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+           });
+}
+
+function get_allPostales() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/AllPostales", async: false })
+        .done(function (result) {
+            var objResult = jQuery.parseJSON(result);
+            var documentType = $("#chkPostale");
+            documentType.html("");
+            iterate_Combo(documentType, objResult);
+        }).fail(function (result) {
+            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+        });
+}
+
+function get_allClientOrigins() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/ClientOrigins", async: false })
+       .done(function (result) {
+           var objResult = jQuery.parseJSON(result);
+           var documentType = $("#chkOriginClient");
+           iterate_Combo(documentType, objResult);
+       }).fail(function (result) {
+           toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+       });
+}
+
+function get_allClientStates() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/ClientStates", async: false })
+        .done(function (result) {
+            var objResult = jQuery.parseJSON(result);
+            var documentType = $("#chkClientState");
+            iterate_Combo(documentType, objResult);
+        }).fail(function (result) {
+            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+        });
+}
+
+function get_allClientType() {
+    $.ajax({ method: "GET", url: "/Finantial/Client/ClientType", async: false })
+        .done(function (result) {
+            var objResult = jQuery.parseJSON(result);
+            var documentType = $("#chkpersonType");
+            iterate_Combo(documentType, objResult);
+        }).fail(function (result) {
+            toastr.error(result.statusText, "Mensaje", { positionClass: "toast-top-full-width" });
+        });
 }
